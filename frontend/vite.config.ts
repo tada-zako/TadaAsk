bcfa06792d6639d91245b5c3d9245837e6ed20e5
tada-zako <tada-zako@outlook.com>
2026-06-25 17:30:25 +0800

    update: 更新 vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import path from "node:path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss(), tsconfigPaths()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
