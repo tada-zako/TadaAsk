64b1bba42dab0a600bf47fd7f27c12fccb17dd51
tada-zako <tada-zako@outlook.com>
2026-06-28 00:13:36 +0800

    deps: 更新前端依赖项
    - 新增 openapi-fetch, openapi-typescript 依赖以及 update-api 脚本命令
    - 降低 typescript 版本到 5.9.3
    - 移除 tsconfig 中的弃用字段 baseUrl

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,12 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), tsconfigPaths()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
+    tsconfigPaths: true,
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
