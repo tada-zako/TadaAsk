f894233471c446be8389259ba6110711f293c91e
tada-zako <tada-zako@outlook.com>
2026-06-28 16:31:13 +0800

    feat: 新增 console router 基础配置，并挂载到 main.ts

--- frontend/src/console/main.ts
+++ frontend/src/console/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import '../shared/styles/style.css'
-import App from './App.vue'
+import "../shared/styles/style.css";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import router from "./router";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
