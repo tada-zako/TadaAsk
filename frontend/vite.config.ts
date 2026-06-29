be412245c5f537d3cebc3ea1a4484a6acb9f9372
tada-zako <tada-zako@outlook.com>
2026-06-30 00:28:35 +0800

    deps: 新增 vue-dev-tools 依赖

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,11 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
 import { defineConfig } from "vite";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [VueDevTools(), vue(), tailwindcss()],
   resolve: {
     tsconfigPaths: true,
     alias: {
